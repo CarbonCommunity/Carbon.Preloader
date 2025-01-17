ebab047ed56f7d43342769d9424d004abf76630d
raul <raulssorban@gmail.com>
2025-01-17 16:21:02 +0100

    Preloading assemblies

--- src/Entrypoint.cs
+++ src/Entrypoint.cs
@@ -10,10 +10,31 @@ namespace Doorstop;
 [SuppressUnmanagedCodeSecurity]
 public sealed class Entrypoint
 {
+	private static readonly string[] PreloadPreUpdate =
+	[
+		Path.Combine(Defines.GetLibFolder(), "0Harmony.dll"),
+		Path.Combine(Defines.GetLibFolder(), "Ben.Demystifier.dll"),
+		Path.Combine(Defines.GetLibFolder(), "ZstdSharp.dll"),
+		Path.Combine(Defines.GetLibFolder(), "SharpCompress.dll")
+	];
+
 	public static void Start()
 	{
 		Defines.Initialize();
 		Config.Init();
+
+		foreach (string file in PreloadPreUpdate)
+		{
+			try
+			{
+				var harmony = Assembly.LoadFile(file);
+				Logger.Log($" Preloaded {harmony.GetName().Name} {harmony.GetName().Version}");
+			}
+			catch (Exception e)
+			{
+				Logger.Log($"Unable to preload '{file}' ({e?.Message})");
+			}
+		}
 
 		if (Config.Singleton.SelfUpdating.Enabled)
 		{
