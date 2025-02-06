f8308e84d9f180db452866fb5955d06847b11d4a
raul <raulssorban@gmail.com>
2025-02-06 01:06:10 +0100

    Stripped irrelevant configuration from Preloader

--- src/Config.cs
+++ src/Config.cs
@@ -12,45 +12,11 @@ public class Config
 {
 	public static Config Singleton;
 
-	public bool DeveloperMode { get; set; } = false;
 	public SelfUpdatingConfig SelfUpdating { get; set; } = new();
-	public PublicizerConfig Publicizer { get; set; } = new();
 
 	public class SelfUpdatingConfig
 	{
 		public bool Enabled { get; set; } = true;
-	}
-
-	public class PublicizerConfig
-	{
-		public string[] PublicizedAssemblies { get; set; } =
-		[
-			"Assembly-CSharp.dll",
-			"Facepunch.Console.dll",
-			"Facepunch.Network.dll",
-			"Facepunch.Nexus.dll",
-			"Rust.Clans.Local.dll",
-			"Rust.Harmony.dll",
-			"Rust.Data.dll"
-		];
-		public string[] PublicizerMemberIgnores { get; set; } =
-		{
-			@"^HiddenValueBase$",
-			@"^HiddenValue`1$",
-			@"^Pool$"
-		};
-
-		public bool IsMemberIgnored(string name)
-		{
-			foreach (var item in PublicizerMemberIgnores)
-			{
-				if (Regex.IsMatch(name, item))
-				{
-					return true;
-				}
-			}
-			return false;
-		}
 	}
 
 	public static void Init()
